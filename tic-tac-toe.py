6c518d020d0589ca1447f2fc6a3d7be25307501b
Doshirakls <128165118+Doshirakls@users.noreply.github.com>
2023-03-20 23:20:45 +0300

    Update tic-tac-toe.py

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,28 +1,37 @@
-def hi():
-    print(" Добро пожаловать ")
-    print(" -----в игру----- ")
-    print(" Крестики--нолики ")
-    print(" ---------------- ")
+# Приветсвие
+def hello():
+    print(" |----------------| ")
+    print(" |Добро пожаловать| ")
+    print(" |-----в игру-----| ")
+    print(" |Крестики--нолики| ")
+    print(" |----------------| ")
 
-field = [[" ", " " ," "] for j in range(3)]
+
+field = [[" ", " ", " "] for g in range(3)]
+# Игровое поле
+
 
 def playing_field():
-    print(f" 0 1 2")
-    for j in range(3):
-        print(f"{j} {field[j][0]} {field[j][1]} {field[j][2]}")
+    print(f"  0 1 2")
+    for g in range(3):
+        print(f"{g} {field[g][0]} {field[g][1]} {field[g][2]}")
+
 
 def game():
+    # Игровые условности
     while True:
         x, y = map(int, input("Время вашего хода: ").split())
         if 0 <= x <= 2 and 0 <= y <= 2:
             if field[x][y] == " ":
                 return x, y
             else:
-                print("Выбранная клетка занята")
+                print("Данная клетка занята")
         else:
-            print("Выбранные координаты вне диапазона игры")
+            print("Выбранные координаты вне диапазона игрового поля")
+
 
 def win():
+    # Условия победы
     cord_of_win = (((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ((2, 0), (2, 1), (2, 2)),
                    ((0, 0), (1, 0), (2, 0)), ((0, 1), (1, 1), (2, 1)), ((0, 2), (1, 2), (2, 2)),
                    ((0, 0), (1, 1), (2, 2)), ((0, 2), (1, 1), (2, 0)))
@@ -31,14 +40,15 @@ def win():
         for i in cords:
             combination.append(field[i[0]][i[1]])
         if combination == ["X", "X", "X"]:
-            print("Выйграл крестик!")
+            print("Победил крестик!")
             return True
         if combination == ["0", "0", "0"]:
-            print("Выйграл нолик!")
+            print("Победил нолик!")
             return True
     return False
 
-hi()
+
+hello()
 move = 0
 while True:
     move += 1
